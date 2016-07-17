b782894486480ea2bbbbf9dc71957f820397401e
Billy Everyteenkinga.rutynowski@gmail.com <kinga.rutynowski@gmail.com>
2016-07-17 15:32:58 -0700

    removed the temp backend

--- db/knex.js
+++
@@ -1,3 +0,0 @@
-var environment = process.env.NODE_ENV || 'development'; //sets up an environment such as testing, staging, development--default  
-var config = require('../knexfile.js')[environment]; //value corresponding to the key of environment 
-module.exports = require('knex')(config); //node knows about knex so you do not have to specify 
